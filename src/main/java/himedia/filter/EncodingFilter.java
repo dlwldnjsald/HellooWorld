4197d8609b34872786fff9f2bf7bae5116a5bf31
dlwldnjsald <163090571+dlwldnjsald@users.noreply.github.com>
2024-06-07 13:21:02 +0900

    filter 연습 0607 13:20

---
+++ src/main/java/himedia/filter/EncodingFilter.java
@@ -0,0 +1,61 @@
+package himedia.filter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Logger;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+public class EncodingFilter implements Filter {
+	
+	//로거 선언
+	private static Logger logger = Logger.getLogger(EncodingFilter.class.getSimpleName());
+	
+	private static String encoding = "UTF-8";
+	
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		
+		logger.info("[EncodingFilter] init");
+		Filter.super.init(filterConfig);
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+		
+		// resp 응답정보로부터 출력을 위한 객체 얻어오기
+		PrintWriter out = resp.getWriter();
+		out.println("<h6>Encoding Filter 작동 전</h6>");
+		
+		// 실제 필터가 처리하는 내용
+		logger.info("[EncodingFilter] doFilter"); //로거 찍기
+		req.setCharacterEncoding(encoding);  //요청 정보의 인코딩 설정 
+		resp.setContentType("text/html"); 	 //응답 정보의 타입 설정
+		resp.setCharacterEncoding(encoding); //응답 정보의 인코딩 설정
+		
+		// chain
+		//요청 처리 종료 후 다음 필터로 전달해준다 
+		chain.doFilter(req, resp); 
+		
+		out.println("<h6>EncodingFilter 작동 후</h6>");
+		
+	}
+	
+	@Override
+	public void destroy() {
+		//Filter.super.destroy(); //필요없음 주석처리해주기
+		
+		logger.info("[EncodingFilter] destroy"); //로거 찍기
+	}
+
+}
+    
+
+  
